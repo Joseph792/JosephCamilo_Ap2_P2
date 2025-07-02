e9e3cb40dd524337cdbc0e61ff7dfa65c54cfeed
Joseph Camilo <joseph_camilo@ucne.edu.do>
2025-07-02 13:27:10 -0400

    Modificando el MainActivity

--- app/src/main/java/edu/ucne/josephcamilo_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/josephcamilo_ap2_p2/MainActivity.kt
@@ -4,7 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.josephcamilo_ap2_p2.presentation.navigation.HostNavigation
 import edu.ucne.josephcamilo_ap2_p2.ui.theme.JosephCamilo_Ap2_P2Theme
 
 @AndroidEntryPoint
@@ -14,6 +21,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JosephCamilo_Ap2_P2Theme {
+                val nav = rememberNavController()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        HostNavigation(nav)
+                    }
+
+                }
             }
         }
     }
