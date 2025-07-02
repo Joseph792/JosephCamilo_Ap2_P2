c450d52ffbb9a3946dbc0fb6ae6a5f6daaddcd70
Joseph Camilo <joseph_camilo@ucne.edu.do>
2025-07-02 12:57:30 -0400

    ApiModule listo

---
+++ app/src/main/java/edu/ucne/josephcamilo_ap2_p2/di/ApiModule.kt
@@ -0,0 +1,35 @@
+package edu.ucne.josephcamilo_ap2_p2.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.josephcamilo_ap2_p2.data.remote.GitHubApi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ApiModule {
+    const val BASE_URL = "https://api.github.com/"
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory()).add(DateAdapter())
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesRepositoryApi(moshi: Moshi): GitHubApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(GitHubApi::class.java)
+    }
+}
