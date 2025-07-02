9d53b29880dfc5ab5b2459a183960febf9640a73
Joseph Camilo <joseph_camilo@ucne.edu.do>
2025-07-02 12:43:50 -0400

    Agregando Hilt

---
+++ app/src/main/java/edu/ucne/josephcamilo_ap2_p2/JosephCamilo_Ap2_P2App.kt
@@ -0,0 +1,9 @@
+package edu.ucne.josephcamilo_ap2_p2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class JosephCamilo_Ap2_P2App : Application(){
+
+}
