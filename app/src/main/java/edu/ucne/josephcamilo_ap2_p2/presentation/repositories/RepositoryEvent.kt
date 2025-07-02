e423d85491fb2ead2fd0eb087fe46208c510fc83
Joseph Camilo <joseph_camilo@ucne.edu.do>
2025-07-02 13:28:24 -0400

    Modificando el RepositoryEvent

--- app/src/main/java/edu/ucne/josephcamilo_ap2_p2/presentation/repositories/RepositoryEvent.kt
+++ app/src/main/java/edu/ucne/josephcamilo_ap2_p2/presentation/repositories/RepositoryEvent.kt
@@ -1,5 +1,8 @@
 package edu.ucne.josephcamilo_ap2_p2.presentation.repositories
 
-interface RepositoryEvent{
-
+sealed interface RepositoryEvent{
+    data object PostRepository: RepositoryEvent
+    data object GetRepositories: RepositoryEvent
+    data object PutRepositories: RepositoryEvent
+    data object DeleteRepositories: RepositoryEvent
 }
