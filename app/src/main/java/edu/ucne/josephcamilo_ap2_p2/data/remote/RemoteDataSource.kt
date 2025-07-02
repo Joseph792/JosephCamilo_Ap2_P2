765dce145b0410c326f9cc1632817073a254a68a
Joseph Camilo <joseph_camilo@ucne.edu.do>
2025-07-02 12:52:31 -0400

    RemoteDataSource listo

---
+++ app/src/main/java/edu/ucne/josephcamilo_ap2_p2/data/remote/RemoteDataSource.kt
@@ -0,0 +1,12 @@
+package edu.ucne.josephcamilo_ap2_p2.data.remote
+
+import edu.ucne.josephcamilo_ap2_p2.data.remote.dto.RepositoryDto
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val gitHubApi: GitHubApi
+) {
+    suspend fun getRepositories(username: String): List<RepositoryDto> {
+        return gitHubApi.listRepos(username)
+    }
+}
