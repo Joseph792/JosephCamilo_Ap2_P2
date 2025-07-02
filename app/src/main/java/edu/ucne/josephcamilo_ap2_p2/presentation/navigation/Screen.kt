db16a787fba1c13139f28f93bf56f6fe623c8eab
Joseph Camilo <joseph_camilo@ucne.edu.do>
2025-07-02 13:24:00 -0400

    Screen listo

---
+++ app/src/main/java/edu/ucne/josephcamilo_ap2_p2/presentation/navigation/Screen.kt
@@ -0,0 +1,11 @@
+package edu.ucne.josephcamilo_ap2_p2.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed class Screen{
+    @Serializable
+    data class Repository(val Id: Int?): Screen()
+
+    @Serializable
+    data object RepositoryList: Screen()
+}
