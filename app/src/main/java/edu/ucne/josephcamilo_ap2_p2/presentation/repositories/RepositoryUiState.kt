7be46fdaf135b030f1ab34726e251e9fc6b525d0
Joseph Camilo <joseph_camilo@ucne.edu.do>
2025-07-02 13:14:02 -0400

    Modificando el repositoryUiState

--- app/src/main/java/edu/ucne/josephcamilo_ap2_p2/presentation/repositories/RepositoryUiState.kt
+++ app/src/main/java/edu/ucne/josephcamilo_ap2_p2/presentation/repositories/RepositoryUiState.kt
@@ -1,2 +1,12 @@
 package edu.ucne.josephcamilo_ap2_p2.presentation.repositories
 
+import edu.ucne.josephcamilo_ap2_p2.data.remote.dto.RepositoryDto
+
+data class RepositoryUiState (
+    val name: String = "",
+    val descripcion: String = "",
+    val html_url: String = "",
+    val isLoading: Boolean = false,
+    val repository: List<RepositoryDto> = emptyList(),
+    val errorMessage: String? = null,
+)
