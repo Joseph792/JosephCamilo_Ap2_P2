0588d74ad91d9e127166b2a3dd24a3d5ea07cca7
Joseph792 <joseph_camilo@ucne.edu.do>
2025-07-09 21:19:58 -0400

    Arreglando Api Module

--- app/src/main/java/edu/ucne/josephcamilo_ap2_p2/di/ApiModule.kt
+++ app/src/main/java/edu/ucne/josephcamilo_ap2_p2/di/ApiModule.kt
@@ -1,4 +1,4 @@
-package edu.ucne.josephcamilo_ap2_p2.data.di
+package edu.ucne.josephcamilo_ap2_p2.di
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -6,7 +6,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import edu.ucne.josephcamilo_ap2_p2.data.remote.GitHubApi
+import edu.ucne.josephcamilo_ap2_p2.data.remote.ViajesApi
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
@@ -14,23 +14,22 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 object ApiModule {
-    private const val BASE_URL = "https://api.github.com/"
+    const val BASE_URL = "https://cswrk042-5108.use2.devtunnels.ms/swagger/index.html"
 
     @Provides
     @Singleton
     fun providesMoshi(): Moshi =
         Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .add(DateAdapter())
+            .add(KotlinJsonAdapterFactory()).add(DateAdapter())
             .build()
 
     @Provides
     @Singleton
-    fun providesRepositoryApi(moshi: Moshi): GitHubApi {
+    fun providesViajesApi(moshi: Moshi): ViajesApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
-            .create(GitHubApi::class.java)
+            .create(ViajesApi::class.java)
     }
 }
