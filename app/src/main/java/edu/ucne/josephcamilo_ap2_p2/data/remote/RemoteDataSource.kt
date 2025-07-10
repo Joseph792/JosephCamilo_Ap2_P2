a01067ebcd2aa638369784acf8cc9af64e8d7ae4
Joseph792 <joseph_camilo@ucne.edu.do>
2025-07-09 21:21:22 -0400

    Arreglando RemoteDataSource

--- app/src/main/java/edu/ucne/josephcamilo_ap2_p2/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/josephcamilo_ap2_p2/data/remote/RemoteDataSource.kt
@@ -1,10 +1,16 @@
 package edu.ucne.josephcamilo_ap2_p2.data.remote
 
-import edu.ucne.josephcamilo_ap2_p2.data.remote.dto.RepositoryDto
+import edu.ucne.josephcamilo_ap2_p2.data.remote.dto.ViajeDto
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
-    private val gitHubApi: GitHubApi
-) {
-    suspend fun getRepository(username: String) = gitHubApi.listRepos(username)
+    private val viajesApi: ViajesApi
+){
+    suspend fun getViajes()= viajesApi.getViajes()
+
+    suspend fun updateViaje(viajeDto: ViajeDto)= viajesApi.updateViaje(viajeDto)
+
+    suspend fun saveViaje(viajeDto: ViajeDto)= viajesApi.saveViaje(viajeDto)
+
+    suspend fun getViaje(id: Int)= viajesApi.getViaje(id)
 }
